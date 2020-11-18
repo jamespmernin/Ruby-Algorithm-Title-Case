35a02662b87d7d1a52738f6b1decbd02d32c4cc9
James Mernin <jamespmernin@gmail.com>
2020-11-18 17:04:53 -0500

    James Mernin - solved algorithm

--- title_case.rb
+++ title_case.rb
@@ -1,7 +1,8 @@
 # RUBY TITLE CASE
+# Solution found here: https://stackoverflow.com/questions/13520162/ruby-capitalize-every-word-first-letter
 
 def title_case(str)
-
+  str.split.map(&:capitalize).join(' ')
 end
 
 puts title_case "I'm a little tea pot"
